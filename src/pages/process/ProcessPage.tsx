3abd6f86ac6d259bb08493ab8323a382d9cb655e
Nielam Dass <nielam.dass@gmail.com>
2026-05-04 20:42:43 -0400

    Update ProcessPage to include more info

--- src/pages/process/ProcessPage.tsx
+++ src/pages/process/ProcessPage.tsx
@@ -1,9 +1,10 @@
 import type { JSX } from "react"
-import { useNavigate, useParams } from "react-router"
+import { Link, useNavigate, useParams } from "react-router"
 import { useTasqUserData } from "../../components/TasqUserData"
 import type Process from "../../types/Process"
 import type Task from "../../types/Task"
-import { Box, Button, Typography } from "@mui/material"
+import { Box, Button, List, ListItem, Typography } from "@mui/material"
+import type Sequence from "../../types/Sequence"
 
 /**
  * Component to display process information
@@ -17,7 +18,8 @@ function ProcessPage(): JSX.Element {
 
   const process: Process = userData.processes[procId]
   if (!process) return <></>
-  const currTask: Task = userData.sequences[process.parentSequenceId].tasks[process.currentTaskIndex]
+  const currSequence: Sequence = userData.sequences[process.parentSequenceId]
+  const currTask: Task = currSequence.tasks[process.currentTaskIndex]
 
   const handleDelete = (processId: string): void => {
     dispatch({actionType: "DELETE-PROCESS", payload: processId})
@@ -28,10 +30,18 @@ function ProcessPage(): JSX.Element {
     <>
       <Button onClick={() => navigate("/")}>&lt;&lt; Back to dashboard</Button>
       <Box sx={{my: 1}}>
-        <Typography variant="h3">Process Details</Typography>
-        <Typography variant="h6">Process ID: {procId}</Typography>
-        <Typography variant="h6">Process Name: {process.processName}</Typography>
+        <Typography variant="h3">Process Details: {process.processName}</Typography>
+        <Typography variant="h6">
+          From sequence:{" "}
+          <Typography variant="inherit" component={Link} to={`/sequence/${currSequence.sequenceId}`} sx={{color: "black"}}>
+            {userData.sequences[process.parentSequenceId].sequenceName}
+          </Typography>
+        </Typography>
         <Typography variant="h6">Current task: {currTask.taskName} {currTask.isOptional && "(Optional)"}</Typography>
+        <Typography variant="h6">Coming Up:</Typography>
+        <List>
+          {currSequence.tasks.slice(process.currentTaskIndex+1, process.currentTaskIndex+6).map((t: Task) => <ListItem key={t.taskId}>{t.taskName} {t.isOptional && "(Optional)"}</ListItem>)}
+        </List>
       </Box>
       <Box sx={{display: "flex", gap: 1, mb: 2}}>
         <Button variant="outlined" onClick={() => dispatch({actionType: "UPDATE-PROCESS", payload: {...process, currentTaskIndex: process.currentTaskIndex - 1}})}>Previous</Button>
